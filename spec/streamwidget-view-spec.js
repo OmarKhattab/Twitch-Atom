050e127fd1385b85bd13e8d80e02f55a98f7af71
omarkhattab <omarkhattab925@gmail.com>
2018-04-10 13:11:37 -0700

    init

---
+++ spec/streamwidget-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StreamwidgetView from '../lib/streamwidget-view';
+
+describe('StreamwidgetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
